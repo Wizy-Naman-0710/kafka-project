d66d25d1711b02c161cc1cc70cc60284bd3fd027
Sarthaksharmassg <sarthaksharmassg@gmail.com>
2025-04-15 23:44:14 +0530

    Added upload_course_resource and get_course_resource functions

--- server.py
+++ server.py
@@ -34,6 +34,25 @@ def login_user(username, password):
         return f"Login successful {result[0]}"  # Returns role (student/instructor)
     return "Error: Invalid credentials"
 
+
+def upload_course_resources(course_id,resource_url,poster_username):
+    try:
+        cursor.exectute(f"INSERT INTO courses (course_id,resource_url,poster_username) values ('{course_id}','{resource_url}','{poster_username}')")
+        conn.commit()
+        return "Resource Added Successfully"
+    return "Error! Unable to Insert"
+def get_course_resource(course_id):
+    try:
+        cursor.execute(f"Select resource_url FROM courses where course_id=?", (course_id,))
+        result=cursor.fetchall()
+        if not result:
+            return "Error: No resources found for this course!"
+        resource_urls = ",".join([row[0] for row in result])
+        return resource_urls
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
 # ✅ Function to Handle Client Requests
 def handle_client(client_socket):
     request = client_socket.recv(1024).decode()
